16eee56b0d1f493cb3c0b6b79457099ca697c9fb
Bigi Admin <admin@bigiawasaana.com>
2026-05-31 17:40:10 -0700

    Update project files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
+
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        display: resolve(__dirname, 'display.html'),
+        login: resolve(__dirname, 'login.html'),
+        order: resolve(__dirname, 'order.html'),
+      },
     },
   },
 });
