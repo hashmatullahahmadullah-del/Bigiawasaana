5544df2f36a8b1184bb7b18b301091e2cef3586d
Bigi Admin <admin@bigiawasaana.com>
2026-06-09 16:12:25 -0700

    style: convert TV menu CSS to pure viewport relative units for flawless scaling across 16-32 inch screens

--- src/tv-menu.js
+++ src/tv-menu.js
@@ -151,12 +151,14 @@ function renderAllCategories() {
     
     const itemsHtml = items.map((item, idx) => `
       <div class="tv-item-card" style="animation-delay: ${idx * 0.05}s">
-        ${item.img 
-          ? `<img src="${item.img}" class="tv-item-img" alt="">` 
-          : `<div class="tv-item-img-placeholder">
-               <svg viewBox="0 0 24 24"><path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/></svg>
-             </div>`
-        }
+        <div class="tv-item-img-wrapper">
+          ${item.img 
+            ? `<img src="${item.img}" class="tv-item-img" alt="">` 
+            : `<div class="tv-item-img-placeholder">
+                 <svg viewBox="0 0 24 24"><path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/></svg>
+               </div>`
+          }
+        </div>
         <div class="tv-item-info">
           <div class="tv-item-header">
             <div class="tv-item-name">${item.name}</div>
