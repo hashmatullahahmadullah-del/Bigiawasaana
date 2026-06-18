6879eba14900a135e073fc2d13d6c7b70b13d681
Bigi Admin <admin@bigiawasaana.com>
2026-06-18 05:50:18 -0700

    fix: disable vite asset hashing so Cloud Function template loads assets correctly

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { resolve } from 'path';
 export default defineConfig({
   build: {
     rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      },
       input: {
         main: resolve(__dirname, 'index.html'),
         admin: resolve(__dirname, 'admin.html'),
